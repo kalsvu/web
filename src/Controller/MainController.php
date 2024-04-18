1a4d61d1d0e43c90ee79a137d7b48767ee61f765
yopkalvu <yopkalvu@gmail.com>
2024-04-18 17:08:56 +0200

    add

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
-
+class MainController extends AbstractController
 {
     #[Route('/test')]
-    public function homepage() : Response
+    public function homepage(): Response
     {
-        return new Response('<strong>Startshop</strong>: your monopoly-blusting option for Starship parts!');
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'KalonGOATBOAT',
+            'class' => 'Garden',
+            'captain' => 'KalonGOAT',
+            'status' => 'Built',
+        ];
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount, 'myShip' => $myShip,
+        ]);
+        #return new Response('<strong>Startshop</strong>: your monopoly-blusting option for Starship parts!');
     }
 }
